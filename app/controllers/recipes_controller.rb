403d22f20cb7362dea47616b60cd01798b0440c2
brytebee <brytebee@gmail.com>
2022-05-16 23:04:05 +0100

    :fire: Cleanup

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -65,6 +65,6 @@ class RecipesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def recipe_params
-      params.require(:recipe).permit(:name, :preparation_time, :cookint_time, :description, :public, :user_id)
+      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
     end
 end
