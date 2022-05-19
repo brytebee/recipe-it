957869cc96aaa76dd7ce9bb5deb029117e6a495a
Okino <37089367+yaseerokino@users.noreply.github.com>
2022-05-19 17:45:21 +0100

    ➕ Create food controller with required methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,26 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
+<<<<<<< Updated upstream
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.recipe_path, alert: exception.message
+=======
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password)
+    end
+  end
+
+  def after_sign_up_path_for(_resource)
+    root_path
+>>>>>>> Stashed changes
   end
 end
