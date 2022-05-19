f6dce6b49c6f8c981e62c290c3e779793b0f661d
brytebee <brytebee@gmail.com>
2022-05-19 10:35:20 +0100

    :white_check_mark: Update migration
    Updated migration with the columns I want to change in the migration file

--- db/migrate/20220519092747_change_time_data_types.rb
+++ db/migrate/20220519092747_change_time_data_types.rb
@@ -1,4 +1,6 @@
 class ChangeTimeDataTypes < ActiveRecord::Migration[7.0]
   def change
+    change_column :recipes, :preparation_time, :decimal
+    change_column :recipes, :cooking_time, :decimal
   end
 end
