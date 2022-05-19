1935e65b9405abddb0b143e18fe9962bae308d51
Okino <37089367+yaseerokino@users.noreply.github.com>
2022-05-19 17:42:41 +0100

    ➕ Create Food model with required validations

---
+++ app/models/food.rb
@@ -0,0 +1,6 @@
+class Food < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :measurement_unit, presence: true
+  validates :price, numericality: { greater_than: 0, less_than: 1_000_000 }
+end
