cf40420096eebbfb9a6d66615f5349421d7a8148
Okino <37089367+yaseerokino@users.noreply.github.com>
2022-05-20 12:58:01 +0100

    ✅ Update views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :recipe_foods
+ 
   resources :foods
   devise_for :users
-  resources :recipes
+  resources :recipes do
+    resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit]
+  end
   root "foods#index"
 end
